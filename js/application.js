e3ba71db4689e0ad5fda5430a8c5bf23dfbe3ea1
Efontser <149145102+Efontser@users.noreply.github.com>
2024-11-03 13:12:59 +0100

    CHANGE index, js, styles
    Minnors updates refered to:
    -delete useless content in index.html
    -link button in application.js
    -added background and font in main.scss

---
+++ js/application.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function (){
+    const boton_buscar = $("#search_city");
+    
+
+    boton_buscar.on("click", function (){
+        // Lógica de búsqueda
+
+
+    })
+
+
+});
