f8bc2ff0347012711201d552eb57bf6fda42725f
Efontser <149145102+Efontser@users.noreply.github.com>
2024-11-04 00:20:22 +0100

    ADD Own location request on application.js
    Asking permisions to the user, requests and shows the own latitude and longitude.

--- js/application.js
+++ js/application.js
@@ -1,7 +1,11 @@
-jQuery(document).ready(function (){
+jQuery(document).ready(function () {
     const search_button = $("#search_city");
     const api_key = 'eba99215f58179a0b0969dfa0bf32f4d';
-    search_button.on("click", function (){
+    const my_location = $('#location');
+
+
+
+    search_button.on("click", function () {
         const city = $("#city_name").val();
         const url_location = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`;
 
@@ -10,7 +14,7 @@ jQuery(document).ready(function (){
             dataType: "json",
             method: 'GET',
             success: function (city_location) {
-                if (city_location){
+                if (city_location) {
                     var latitude = city_location[0].lat;
                     var longitude = city_location[0].lon;
                     var lang = 'sp';
@@ -20,30 +24,51 @@ jQuery(document).ready(function (){
                         url: weather_url,
                         dataType: "json",
                         success: function (weather_response) {
-                            if (weather_response){
-                               var first_forecast = weather_response.list[0].main.temp_min;
-                               alert(`Primer aviso sobre temperatura mínima: ${first_forecast}`);
+                            if (weather_response) {
+                                var first_forecast = weather_response.list[0].main.temp_min;
+                                alert(`Primer aviso sobre temperatura mínima: ${first_forecast}`);
                             }
                         },
-                        error: function (xhr, status){
+                        error: function (xhr, status) {
                             alert(`Something has gone wrong. Status: ${status}; xhr: ${xhr}`)
                         },
-                        complete: function (xhr, status){
+                        complete: function (xhr, status) {
                             alert(`The status of the request is completed: ${status}.\The 'xhr': ${xhr}`)
                         }
                     });
-                }else{
-                    alert(`City ${city}, doesn't founded`);   
+                } else {
+                    alert(`City ${city}, doesn't founded`);
                 }
             },
-            error: function (xhr, status){
+            error: function (xhr, status) {
                 alert(`Something has gone wrong: ${status}`)
             },
-            complete: function (xhr, status){
+            complete: function (xhr, status) {
                 alert(`The status of the request is completed: ${status}.\The 'xhr': ${xhr}`)
             }
         });
-    })
+    });
+
+    // Actual location. 
+    my_location.on('click', function (event) {
+        event.preventDefault();
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                function (position) {
+                    const my_latitude = position.coords.latitude;
+                    const my_longitude = position.coords.longitude;
+                    alert(`Tu ubicación es:\nLatitud: ${my_latitude}, Longitud: ${my_longitude}`);
+                },
+                function(error){
+                    alert(`Error trying to obtain the ubication: ${error.message}`);
+                }
+            );
+        } else {
+            alert('Geolocation not supported in this client/navegator');
+        }
+
+    });
+
 
 
 });
