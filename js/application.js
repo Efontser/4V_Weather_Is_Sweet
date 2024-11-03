aab373098fcfa89c73ea6bbf216d9d78d507a4ca
Efontser <149145102+Efontser@users.noreply.github.com>
2024-11-03 18:30:51 +0100

    FEATURE basic GET request to the API
    The GET request hasn´t been adjusted yet, but now i'm able to obtain a response from the OpenWeather API.

--- js/application.js
+++ js/application.js
@@ -1,11 +1,48 @@
 jQuery(document).ready(function (){
-    const boton_buscar = $("#search_city");
-    
+    const search_button = $("#search_city");
+    const api_key = 'eba99215f58179a0b0969dfa0bf32f4d';
+    search_button.on("click", function (){
+        const city = $("#city_name").val();
+        const url_location = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`;
 
-    boton_buscar.on("click", function (){
-        // Lógica de búsqueda
-
-
+        $.ajax({
+            url: url_location,
+            dataType: "json",
+            method: 'GET',
+            success: function (city_location) {
+                if (city_location){
+                    var latitude = city_location[0].lat;
+                    var longitude = city_location[0].lon;
+                    var lang = 'sp';
+                    weather_url = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${api_key}&units=metric&lang=es`;
+                    $.ajax({
+                        type: "GET",
+                        url: weather_url,
+                        dataType: "json",
+                        success: function (weather_response) {
+                            if (weather_response){
+                               var first_forecast = weather_response.list[0].main.temp_min;
+                               alert(`Primer aviso sobre temperatura mínima: ${first_forecast}`);
+                            }
+                        },
+                        error: function (xhr, status){
+                            alert(`Something has gone wrong. Status: ${status}; xhr: ${xhr}`)
+                        },
+                        complete: function (xhr, status){
+                            alert(`The status of the request is completed: ${status}.\The 'xhr': ${xhr}`)
+                        }
+                    });
+                }else{
+                    alert(`City ${city}, doesn't founded`);   
+                }
+            },
+            error: function (xhr, status){
+                alert(`Something has gone wrong: ${status}`)
+            },
+            complete: function (xhr, status){
+                alert(`The status of the request is completed: ${status}.\The 'xhr': ${xhr}`)
+            }
+        });
     })
 
 
