151363d25e0596c8faa108de82e21a97b0f48619
Efontser <149145102+Efontser@users.noreply.github.com>
2024-11-05 12:07:58 +0100

    UPDATE 'my_location.on()' CHANGE 'Get request'
    UPDATED: Added Get request to OpenWeather using my location.
    
    CHANGED: Now we get the weather by the City's name. Which is more accurate when you are asking by country or region instead of City.

--- js/application.js
+++ js/application.js
@@ -4,60 +4,63 @@ jQuery(document).ready(function () {
     const my_location = $('#location');
 
 
-
     search_button.on("click", function () {
         const city = $("#city_name").val();
-        const url_location = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`;
-
+        const weather_url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}&units=metric&lang=es`;
         $.ajax({
-            url: url_location,
+            url: weather_url,
             dataType: "json",
             method: 'GET',
-            success: function (city_location) {
-                if (city_location) {
-                    var latitude = city_location[0].lat;
-                    var longitude = city_location[0].lon;
-                    weather_url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${api_key}&units=metric&lang=es`;
-                    $.ajax({
-                        type: "GET",
-                        url: weather_url,
-                        dataType: "json",
-                        success: function (weather_response) {
-                            if (weather_response) {
-                                var first_forecast = weather_response.list[0].main.temp_min;
-                                alert(`Primer aviso sobre temperatura mínima: ${first_forecast}`);
-                                const city_name = weather_response.city.name
-                                alert(city_name)
-                            }
-                        },
-                        error: function (xhr, status) {
-                            alert(`Something has gone wrong. Status: ${status}; xhr: ${xhr}`)
-                        },
-                        complete: function (xhr, status) {
-                            alert(`The status of the request is completed: ${status}.\The 'xhr': ${xhr}`)
-                        }
-                    });
+            success: function (weather_response) {
+                if (weather_response) {
+                    var first_forecast = weather_response.list[0].main.temp_min;
+                    alert(`Primer aviso sobre temperatura mínima: ${first_forecast}`);
+                    const city_name = weather_response.city.name;
+                    alert(city_name);
                 } else {
                     alert(`City ${city}, doesn't founded`);
                 }
             },
             error: function (xhr, status) {
-                alert(`Something has gone wrong: ${status}`)
+                alert(`Something has gone wrong: ${status}`);
             },
             complete: function (xhr, status) {
-                alert(`The status of the request is completed: ${status}.\The 'xhr': ${xhr}`)
+                alert(`The status of the request is completed: ${status}.\The 'xhr': ${xhr}`);
             }
         });
     });
 
     // Actual location. 
     my_location.on('click', function (event) {
+        // Prevent the load of the page
         event.preventDefault();
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
                 function (position) {
                     const my_latitude = position.coords.latitude;
                     const my_longitude = position.coords.longitude;
+                    const location_url = `https://api.openweathermap.org/data/2.5/forecast?lat=${my_latitude}&lon=${my_longitude}&appid=${api_key}&units=metric&lang=es`;
+                    $.ajax({
+                        url: location_url,
+                        dataType: "json",
+                        method: 'GET',
+                        success: function (weather_response) {
+                            if (weather_response) {
+                                var first_forecast = weather_response.list[0].main.temp_min;
+                                alert(`Primer aviso sobre temperatura mínima: ${first_forecast}`);
+                                const city_name = weather_response.city.name
+                                alert(city_name)
+                            } else {
+                                alert(`City ${city}, doesn't founded`);
+                            }
+                        },
+                        error: function (xhr, status) {
+                            alert(`Something has gone wrong: ${status}`)
+                        },
+                        complete: function (xhr, status) {
+                            alert(`The status of the request is completed: ${status}.\The 'xhr': ${xhr}`)
+                        }
+                    });
                     alert(`Tu ubicación es:\nLatitud: ${my_latitude}, Longitud: ${my_longitude}`);
                 },
                 function(error){
