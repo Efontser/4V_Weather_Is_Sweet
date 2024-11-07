8e3160051437995973bbddf306c669f608c27313
Efontser <149145102+Efontser@users.noreply.github.com>
2024-11-07 22:43:00 +0100

    ADD build_my_dict() in application.js
    Builds a dict taking the json data

--- js/application.js
+++ js/application.js
@@ -1,8 +1,15 @@
 jQuery(document).ready(function () {
+    // HTML Items
     const search_button = $("#search_city");
-    const api_key = 'eba99215f58179a0b0969dfa0bf32f4d';
     const my_location = $('#location');
+    const section_show_weather = $('.show-section');
+    const day_list = $("#day_list");
 
+    // Dict customized
+    let dict_weather = {} 
+
+    // API Key
+    const api_key = 'eba99215f58179a0b0969dfa0bf32f4d';
 
     search_button.on("click", function () {
         const city = $("#city_name").val();
@@ -13,16 +20,20 @@ jQuery(document).ready(function () {
             method: 'GET',
             success: function (weather_response) {
                 if (weather_response) {
+                    dict_weather = build_my_dict(weather_response);
+                    section_show_weather.show();
                     var first_forecast = weather_response.list[0].main.temp_min;
                     alert(`Primer aviso sobre temperatura mínima: ${first_forecast}`);
                     const city_name = weather_response.city.name;
                     alert(city_name);
+                    
                 } else {
                     alert(`City ${city}, doesn't founded`);
                 }
             },
             error: function (xhr, status) {
                 alert(`Something has gone wrong: ${status}`);
+                section_show_weather.hide();
             },
             complete: function (xhr, status) {
                 alert(`The status of the request is completed: ${status}.\The 'xhr': ${xhr}`);
@@ -46,6 +57,8 @@ jQuery(document).ready(function () {
                         method: 'GET',
                         success: function (weather_response) {
                             if (weather_response) {
+                                dict_weather = build_my_dict(weather_response);
+                                section_show_weather.show();
                                 var first_forecast = weather_response.list[0].main.temp_min;
                                 alert(`Primer aviso sobre temperatura mínima: ${first_forecast}`);
                                 const city_name = weather_response.city.name
@@ -55,7 +68,8 @@ jQuery(document).ready(function () {
                             }
                         },
                         error: function (xhr, status) {
-                            alert(`Something has gone wrong: ${status}`)
+                            alert(`Something has gone wrong: ${status}`);
+                            section_show_weather.hide();
                         },
                         complete: function (xhr, status) {
                             alert(`The status of the request is completed: ${status}.\The 'xhr': ${xhr}`)
@@ -73,6 +87,48 @@ jQuery(document).ready(function () {
 
     });
 
+    function dayOfWeek(timestampUnix){
+        let dayName = null;
+        if (timestampUnix){
+            // Miliseconds to Date()
+            const date = new Date(timestampUnix * 1000);
+            // Get day name
+            const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            dayName = daysOfWeek[date.getUTCDay()]; 
+        }
+        dayName = (dayName != null) ? dayName : "Any day";
+        return dayName;
+    }
 
+    // Function that uses the Data Json, and tries to customice a dictionary in order to paint it in the HTML.
+    // Returns --> The dictionary customiced, so it may be assigned to a global variable.
+    function build_my_dict(weather){
+       let dict_weather_aux = {}
+        for (let time_stamp_report of weather["list"]){
+            let dict_own_weather = {};
+            let keys_atmospheric_factors = ["temp", "temp_min", "temp_max", "pressure", "humidity"];
+            let main = time_stamp_report["main"];
+            for (let key in main){
+                if (keys_atmospheric_factors.includes(key)) dict_own_weather[key] = main[key];
+            }
+            let keys_weather_icon = ["main", "icon"];
+            // For an unknown reason, we must iterate over a dict with a list with only one element...
+            weather_info = time_stamp_report["weather"][0];
+            for (let key in weather_info){
+                if (keys_weather_icon.includes(key)) dict_own_weather[key] = weather_info[key];
+            }
+            dict_own_weather["dt_txt"] = time_stamp_report["dt_txt"];
 
+            // Get the weekDay. Wheter is Monday, Tuesday... So we don't override the data.
+            let my_day = dayOfWeek(time_stamp_report["dt"]);
+            if (dict_weather_aux[my_day]){
+                dict_weather_aux[my_day].push(dict_own_weather);
+            }else {
+                // There's no day yet. So we 'save' the KEY too.
+                dict_weather_aux[my_day] = dict_own_weather;
+            }
+        }
+        return dict_weather_aux;
+    }
 });
+
